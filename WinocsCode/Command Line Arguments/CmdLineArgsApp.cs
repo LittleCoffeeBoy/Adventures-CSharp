f4b4126987870afa7ce6356af50dc3cd78fa5482
Douglas Silva <littlecoffeeboy@gmail.com>
2017-12-20 18:38:50 -0300

    Command Line Arguments
    Testing some command line arguments.

---
+++ WinocsCode/Command Line Arguments/CmdLineArgsApp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+// Reference: https://docs.microsoft.com/pt-br/dotnet/csharp/programming-guide/main-and-command-args/command-line-arguments
+// This application needs a numeric argument.
+class CmdLineArgsApp
+{
+    static int Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            PrintHelp();
+            Console.ReadKey();
+            return 1;
+        }
+
+        int number;
+        bool test = int.TryParse(args[0], out number);
+        if (!test)
+        {
+            PrintHelp();
+            Console.ReadKey();
+            return 1;
+        }
+
+        long result = Functions.Factorial(number);
+        if (result == -1)
+        {
+            Console.WriteLine("Input must be >= 0 and <= 20.");
+        }
+        else
+        {
+            Console.WriteLine($"The Factorial of {number} is {result}");
+        }
+
+        Console.Write("Press any key to exit.");
+        Console.ReadKey();
+        return 0;
+    }
+
+    static void PrintHelp()
+    {
+        Console.WriteLine("Please enter a numeric argument.");
+        Console.WriteLine("Usage: Factorial <number>");
+    }
+}
---
+++ WinocsCode/Command Line Arguments/Functions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+// Reference: https://docs.microsoft.com/pt-br/dotnet/csharp/programming-guide/main-and-command-args/command-line-arguments
+
+public class Functions
+{
+    public static long Factorial(int number)
+    {
+        if ((number < 0) || (number > 20))
+        {
+            return -1;
+        }
+
+        long tempResult = 1;
+        for (int i = 1; i <= number; i++)
+        {
+            tempResult *= i;
+        }
+
+        return tempResult;
+    }
+}
